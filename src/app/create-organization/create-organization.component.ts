22661c27e426433501e50059abbb1c164325d55c
swapnilsharmacovetus <swapnil.sharma@covetus.com>
2024-04-06 16:47:52 +0530

    Create organization page and validation in login page

--- src/app/create-organization/create-organization.component.ts
+++ src/app/create-organization/create-organization.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { CommonApiService } from '../core/services/common-api.service';
 
 @Component({
   selector: 'app-create-organization',
@@ -7,4 +10,49 @@ import { Component } from '@angular/core';
 })
 export class CreateOrganizationComponent {
 
+  organizationForm:FormGroup;
+  isFieldsValid:boolean = false;
+  fileTypeBase64!: ArrayBuffer | any;
+
+  constructor(private formBuild:FormBuilder,private toastr:ToastrService,private serviceApi:CommonApiService){
+    this.organizationForm = this.formBuild.group({
+      name: ['',[Validators.required, Validators.minLength(2), Validators.pattern(/^(?!(SELECT|INSERT|UPDATE|DELETE|DROP|UNION|EXEC|ALTER|CREATE|TRUNCATE)|(['";\\])|(\b\d+\b)|(\/\*[\s\S]*?\*\/|--.*)|(AND|OR|NOT|XOR)|\b(?:SELECT|INSERT|UPDATE|DELETE|EXEC)\s*\(|(error|exception|warning))/i)]],
+      about:['',[Validators.required, Validators.maxLength(150) ,Validators.pattern(/^(?!(SELECT|INSERT|UPDATE|DELETE|DROP|UNION|EXEC|ALTER|CREATE|TRUNCATE)|(['";\\])|(\b\d+\b)|(\/\*[\s\S]*?\*\/|--.*)|(AND|OR|NOT|XOR)|\b(?:SELECT|INSERT|UPDATE|DELETE|EXEC)\s*\(|(error|exception|warning))/i)]],
+      website: ['', [Validators.required, Validators.pattern('(^((http|https)://)|((www)[.]))[A-Za-z0-9_@./#!$%^:*&+-]+([\-\.]{1}[a-z0-9]+)*\.(?:com|net|in|org|io)$')]],
+      phone: ['', [Validators.required, Validators.pattern('[6-9][0-9]{12}')]],
+      mobile: ['', [Validators.required, Validators.pattern('[6-9][0-9]{12}')]],
+      fax: ['', [Validators.required,Validators.pattern('^[0-9]*$'), Validators.minLength(10), Validators.maxLength(13)]],
+      street:['',[Validators.required, Validators.pattern(/^(?!(SELECT|INSERT|UPDATE|DELETE|DROP|UNION|EXEC|ALTER|CREATE|TRUNCATE)|(['";\\])|(\b\d+\b)|(\/\*[\s\S]*?\*\/|--.*)|(AND|OR|NOT|XOR)|\b(?:SELECT|INSERT|UPDATE|DELETE|EXEC)\s*\(|(error|exception|warning))/i)]],
+      city:['',[Validators.required, Validators.pattern(/^(?!(SELECT|INSERT|UPDATE|DELETE|DROP|UNION|EXEC|ALTER|CREATE|TRUNCATE)|(['";\\])|(\b\d+\b)|(\/\*[\s\S]*?\*\/|--.*)|(AND|OR|NOT|XOR)|\b(?:SELECT|INSERT|UPDATE|DELETE|EXEC)\s*\(|(error|exception|warning))/i)]],
+      country:['',[Validators.required, Validators.pattern(/^(?!(SELECT|INSERT|UPDATE|DELETE|DROP|UNION|EXEC|ALTER|CREATE|TRUNCATE)|(['";\\])|(\b\d+\b)|(\/\*[\s\S]*?\*\/|--.*)|(AND|OR|NOT|XOR)|\b(?:SELECT|INSERT|UPDATE|DELETE|EXEC)\s*\(|(error|exception|warning))/i)]],
+      state:['',[Validators.required, Validators.pattern(/^(?!(SELECT|INSERT|UPDATE|DELETE|DROP|UNION|EXEC|ALTER|CREATE|TRUNCATE)|(['";\\])|(\b\d+\b)|(\/\*[\s\S]*?\*\/|--.*)|(AND|OR|NOT|XOR)|\b(?:SELECT|INSERT|UPDATE|DELETE|EXEC)\s*\(|(error|exception|warning))/i)]],
+      zip:['',[Validators.required,Validators.minLength(6), Validators.maxLength(6), Validators.pattern('^[0-9]*$')]],
+      logo:['',[Validators.required]]
+    });
+  }
+
+  get formData() { return this.organizationForm.controls }
+
+  onSubmit(){
+    if(this.organizationForm.invalid) {
+      this.isFieldsValid = true;
+      return
+    }
+    this.organizationForm.value.logo = this.fileTypeBase64;
+    this.serviceApi.allPostMethod("accounts/account",this.organizationForm.value).subscribe((response:any)=>{
+      if(response.message){
+        this.toastr.success("Form Submitted","",{closeButton:true,timeOut:5000});
+      }else{
+        this.toastr.error("Something went wrong","",{timeOut:5000,closeButton:true});
+      }
+    })
+  }
+
+  convertImageToBase64(file_event: any) {
+    const reader = new FileReader();
+    reader.readAsDataURL(file_event.srcElement.files[0]);
+    reader.onload = () => {
+      this.fileTypeBase64 = reader.result
+    };
+  }
 }
