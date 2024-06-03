2240bd819184ceaeaf5f682bc9238a4089abc87e
arunyadav7059 <arunyadav.covetus@gmail.com>
2024-06-03 19:12:03 +0530

    change in role permission module and interceptor

--- src/app/super-admin/role-permission/role-permission.component.ts
+++ src/app/super-admin/role-permission/role-permission.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { map } from 'rxjs';
 import { CommonApiService } from '../../core/services/common-api.service';
 import { CommunicateService } from '../../core/services/communicate.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-role-permission',
@@ -12,8 +13,12 @@ export class RolePermissionComponent {
  modules:any = [];
  roles: any = []
  rolePermission : any = [];
+ popContent: string = '';
+ popTitle: string = '';
+ allPemissions:any = [];
+ 
 
-  constructor(private api : CommonApiService, private communicate: CommunicateService,){
+  constructor(private api : CommonApiService, private communicate: CommunicateService,private toastr: ToastrService){
     this.getRolePermissionList()
     this.getModuleList();
     this.getRoles();
@@ -21,7 +26,14 @@ export class RolePermissionComponent {
 
   getRoles(){
     this.communicate.isLoaderLoad.next(true);
-    this.api.allPostMethod('role/roles',{}).subscribe({
+    this.api.allPostMethod('role/roles',{})
+    .pipe(map((res:any)=>{
+       res.data.forEach((role:any)=>{
+          role = this.checkSelectAll(role);
+       })
+       return res
+    }))
+    .subscribe({
       next: (res:any)=>{
           if(!res.error){
             this.communicate.isLoaderLoad.next(false);
@@ -56,6 +68,7 @@ export class RolePermissionComponent {
           if(!res.error){
             this.communicate.isLoaderLoad.next(false);
             this.modules = res.data;
+            this.allPemissions = res.data.flatMap((m:any) => m.section_masters).flatMap((s:any)=> s.permission_masters);
           }else{
             this.communicate.isLoaderLoad.next(false);
              console.log(res.error.message || res.error)
@@ -87,29 +100,53 @@ export class RolePermissionComponent {
     this.api.allPostMethod('rolepermission/addeditrolepermission',payload).subscribe({
       next: (res:any)=>{
            if(!res.error){
-            this.communicate.isLoaderLoad.next(false);
-               console.log('successfully');
+            this.toastr.success(res.message,"",{closeButton:true,timeOut:2000}).onHidden.subscribe(()=>{
+              this.communicate.isLoaderLoad.next(false);
+            });           
            }else{
-            this.communicate.isLoaderLoad.next(false);
-              console.log(res.error.message || res.errror)
+            this.toastr.error(res.message || res.error,"",{closeButton:true,timeOut:3000}).onHidden.subscribe(()=>{
+              this.communicate.isLoaderLoad.next(false);
+            });
            }
       }
     })
   }
 
-  changeBox(roleId:any,permission:any){
-    let index = this.rolePermission.findIndex((v:any) => v.role_id == roleId && v.section_id == permission.section_id && v.module_id == permission.module_id && v.permission_id == permission.id);
+  changeBox(role:any,permission:any){
+    let index = this.rolePermission.findIndex((v:any) => v.role_id == role.id && v.section_id == permission.section_id && v.module_id == permission.module_id && v.permission_id == permission.id);
      if(index == -1){
-       this.rolePermission.push({role_id: roleId,module_id: permission.module_id, section_id: permission.section_id, permission_id: permission.id})
+       this.rolePermission.push({role_id: role.id,module_id: permission.module_id, section_id: permission.section_id, permission_id: permission.id})
      }else{
          if(this.rolePermission[index].id){
            this.rolePermission[index]['deleted'] = !this.rolePermission[index]['deleted'];
          }else{
           this.rolePermission.splice(index,1)
          }
-     }
+        }
+     this.checkSelectAll(role)   
     }
+
     checkPermission(roleId:any,permission:any){
       return this.rolePermission.findIndex((v:any) =>  v.deleted != 1 && v.role_id == roleId && v.section_id == permission.section_id && v.module_id == permission.module_id && v.permission_id == permission.id) != -1
+    }
+    
+    checkAll(role:any){
+      role['selectAll'] = !role['selectAll'];
+      this.allPemissions.forEach((p:any)=>{
+        let index = this.rolePermission.findIndex((v:any) => v.role_id == role.id && v.section_id == p.section_id && v.module_id == p.module_id && v.permission_id == p.id);
+
+        if(index !== -1 && this.rolePermission[index].id){
+          this.rolePermission[index]['deleted'] =  !role['selectAll'];
+        }else if(role['selectAll'] && index == -1){
+            this.rolePermission.push({role_id: role.id,module_id: p.module_id, section_id: p.section_id, permission_id: p.id})
+        }else if(!role['selectAll'] && index !== -1){
+             this.rolePermission.splice(index,1)
+        }
+      })
+      console.log(this.rolePermission)
+    }
+
+    checkSelectAll(role:any){
+      role['selectAll'] = this.rolePermission.filter((rp:any) => rp.role_id == role.id && !rp.deleted).length == this.allPemissions.length;
     }
 }
