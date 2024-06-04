f8a36c3ce7ea3710e54b74e87d940ea5a92d76b9
shantanusherke <ssherke.covetus@gmail.com>
2024-06-04 14:29:13 +0530

    scroll team form

--- src/app/dashboard-detail/user-detail/user-detail.component.ts
+++ src/app/dashboard-detail/user-detail/user-detail.component.ts
@@ -14,9 +14,10 @@ export class UserDetailComponent {
   Date = new Date();
   current_role: any;
   user_data: any;
-  searchValue:string= ''
+  searchValue: string = ''
   searchData = new Subject<Event>();
-  reqObj : any;
+  reqObj: any;
+  totalPages: number = 0;
   constructor(private api: CommonApiService, private communicate: CommunicateService, private toastr: ToastrService) {
     this.current_role = localStorage.getItem('role');
     this.current_role = JSON.parse(this.current_role);
@@ -24,30 +25,31 @@ export class UserDetailComponent {
     this.user_data = localStorage.getItem('Shared_Data');
     this.user_data = JSON.parse(this.user_data);
     this.reqObj = {
-      account_id : this.user_data.account_id,
-      pageNumber: 1, 
+      account_id: this.user_data.account_id,
+      pageNumber: 1,
       pageSize: 10,
       keyword: ''
     }
     this.getUserList();
   }
 
-  ngOnInit(){
-    this.searchData.pipe(filter((x:any)=> x.length >=3 || x == ''),debounceTime(1000),distinctUntilChanged()).
-    subscribe((data:any)=>{
-      this.user_list = [];
-      this.reqObj.pageNumber = 1;
-      this.reqObj.keyword = data;
-      this.getUserList();
-    });
+  ngOnInit() {
+    this.searchData.pipe(filter((x: any) => x.length >= 3 || x == ''), debounceTime(1000), distinctUntilChanged()).
+      subscribe((data: any) => {
+        this.user_list = [];
+        this.reqObj.pageNumber = 1;
+        this.reqObj.keyword = data;
+        this.getUserList();
+      });
   }
 
   getUserList() {
     this.communicate.isLoaderLoad.next(true);
     this.api.allPostMethod("users/getUserList", this.reqObj).subscribe((getUser: any) => {
       if (getUser.data.length > 0) {
-        this.user_list = [...this.user_list,...getUser.data];
-      } 
+        this.user_list = [...this.user_list, ...getUser.data];
+        this.totalPages = getUser['totalPages'];
+      }
       this.communicate.isLoaderLoad.next(false);
     })
   }
@@ -65,8 +67,10 @@ export class UserDetailComponent {
     });
   }
 
-  onScroll(){
-    this.reqObj.pageNumber += 1;
-    this.getUserList();
+  onScroll() {
+    if (this.reqObj.pageNumber < this.totalPages) {
+      this.reqObj.pageNumber += 1;
+      this.getUserList();
+    }
   }
 }
